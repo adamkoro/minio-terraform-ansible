2848b37929d887fe57c53982dee2a0c513a7f29b
adamkoro <adam.koronics@gmail.com>
2023-03-15 19:15:37 +0100

    feat: minio and proxy terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,56 @@
+variable "minio_count" {
+  type = number
+}
+variable "minio_domain_name" {
+  type = string
+}
+variable "proxy_count" {
+  type = number
+}
+variable "proxy_domain_name" {
+  type = string
+}
+
+variable "cloud_init_username" {
+  type = string
+}
+variable "cloud_init_password" {
+  type = string
+}
+variable "cloud_init_sshkey" {
+  type = string
+}
+variable "cloud_init_nameserver" {
+  type = string
+}
+variable "cloud_init_search_domain" {
+  type = string
+}
+variable "cloud_init_netmask" {
+  type = string
+}
+variable "cloud_init_gateway" {
+  type = string
+}
+variable "node_ip_range" {
+  type = string
+}
+variable "ssh_private_key_path" {
+  type = string
+}
+
+variable "base_root_volume_path" {
+  type = string
+}
+variable "root_volume_pool" {
+  type = string
+}
+variable "swap_volume_pool" {
+  type = string
+}
+variable "data_volume_pool" {
+  type = string
+}
+variable "minio_data_path" {
+  type = string
+}
