57b63426a30858148da17457186b17e4fa8b6b0e
adamkoro <adam.koronics@gmail.com>
2023-06-12 23:29:22 +0200

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,12 +3,13 @@ terraform {
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.7.0"
+      version = "0.7.1"
     }
   }
 }
 
 provider "libvirt" {
-  uri = "qemu:///system"
+  #uri = "qemu:///system"
+  uri = "qemu+ssh://kvm-1/system"
 }
 
